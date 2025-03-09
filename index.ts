b20cb1b377fb5be25668baf78e9dee77e9df5e13
Donald Roman <romanunuose@gmail.com>
2025-03-10 00:25:19 +0100

    serving the static files using just node.js via http

--- index.ts
+++ index.ts
@@ -1,26 +1,39 @@
 import * as http from "http";
-// import fs from "fs";
-// import url from "url"
+import * as fs from "fs";
 
 http.createServer((req, res) => {
   const url = req.url as string;
 
-  if (url.endsWith('favicon.ico')) {
+  if (url == "/") {
+    return res.end();
+  } else if (url.endsWith("favicon.ico")) {
     res.writeHead(404, { "content-type": "text/plain" });
-    return res.end('File not found', 'utf-8');
+    return res.end("File not found", "utf-8");
   }
 
-  const extension = url.split('.').at(-1);
-  console.log(extension);
+  const extension = url.split(".").at(-1);
+  const fileContent = fs.readFileSync(url.slice(1), {
+    encoding: extension === "jpg" ? null : "utf-8"
+  });
 
-
-  return res.end();
-}).listen(8080, "localhost", () => {
-  console.log('Server running at http://localhost:8080');
+  res.writeHead(200, { "content-type": getMimeType(extension) });
+  res.end(fileContent);
 })
+.listen(8080, "localhost", () => {
+  console.log("Server running at http://localhost:8080");
+});
 
-
-// res.writeHead(200, { "Content-Type": "text/html" });
-// fs.createReadStream("index.html").pipe(res);
-
-// function getMimeType
+function getMimeType(extension: string | undefined) {
+  switch (extension) {
+    case "html":
+      return "text/html";
+    case "css":
+      return "text/css";
+    case "js":
+      return "text/javascript";
+    case "jpg":
+      return "image/jpeg";
+    default:
+      return "application/octet-stream";
+  }
+}
