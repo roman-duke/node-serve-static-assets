abd83fdd98ecbbdbd5d01d2f2bff75de8c6fd15e
Donald Roman <romanunuose@gmail.com>
2025-03-11 13:23:40 +0100

    feat: basic streaming of video file

--- index.ts
+++ index.ts
@@ -1,42 +1,81 @@
+import * as http from "http";
 import * as fs from "fs";
 
-const pathToFile = process.argv[2] || '/Users/donaldroman/Downloads/Proko Portrait Drawing Fundamentals/Portrait Drawing/18_drawing_morgan_1280x720.mp4';
-const destinationPath = process.argv[3] || 'outputFile.mp4';
+http.createServer((req, res) => {
+  const url = req.url as string;
 
-const readable = fs.createReadStream(pathToFile);
-const writable = fs.createWriteStream(destinationPath);
-
-fs.stat(pathToFile, (err, stats) => {
-  const fileSize = stats.size;
-  const fileArray = pathToFile.split('.');
-  let counter = 1;
-  let duplicate = '';
-
-  try {
-    duplicate = destinationPath + '/' + fileArray[0] + '_copy.' + fileArray.at(-1);
-  } catch {
-    console.error('Invalid file name!');
+  if (url == "/") {
+    return res.end();
+  } else if (url.endsWith("favicon.ico")) {
+    res.writeHead(404, { "content-type": "text/plain" });
+    return res.end("File not found", "utf-8");
   }
 
-  process.stdout.write(`File: ${duplicate} is being created`);
+  const extension = url.split(".").at(-1);
+  const filePath = url.slice(1);
 
-  readable.on("data", (chunk) => {
-    const percentageCopied = ((chunk.length * counter) / fileSize) * 100;
-    process.stdout.clearLine(0);
-    process.stdout.cursorTo(0);
-    process.stdout.write(`${Math.round(percentageCopied)}%`);
-    counter += 1;
-  });
+  if (extension !== "mp4") {
+    // synchronously read our static files because they are really small in size.
+    const fileContent = fs.readFileSync(filePath, {
+      encoding: extension == "jpg" ? undefined : "utf-8",
+    });
+    res.writeHead(200, { "content-type": getMimeType(extension) });
+    res.end(fileContent);
+  } else {
+    fs.stat(filePath, (err, stats) => {
+      if (err) res.end(err);
+      const range = req.headers.range;
+      if (!range) {
+        // 416 Wrong range
+        return res.writeHead(416, "Invalid range");
+      }
 
-  readable.on('error', (e) => {
-    console.log('Unexpected error has occurred.', e);
-  });
+      const fileSize = stats.size;
+      const { start, end, chunkSize } = getStreamArguments(range, fileSize);
 
-  readable.pipe(writable);
+      res.writeHead(206, {
+        "content-range": `bytes ${start}-${end}/${fileSize}`,
+        "accept-ranges": "bytes",
+        "content-length": chunkSize,
+        "content-type": getMimeType(extension),
+      });
 
-  writable.on('finish', () => {
-    process.stdout.clearLine(0);
-    process.stdout.cursorTo(0);
-    process.stdout.write('Successfully created the file copy!');
-  });
+      fs.createReadStream(filePath, {
+        start: start,
+        end: end,
+      }).pipe(res);
+    });
+  }
+
 })
+.listen(8080, "localhost", () => {
+  console.log("Server running at http://localhost:8080");
+});
+
+function getMimeType(extension: string | undefined) {
+  switch (extension) {
+    case "html":
+      return "text/html";
+    case "css":
+      return "text/css";
+    case "js":
+      return "text/javascript";
+    case "jpg":
+      return "image/jpeg";
+    case "mp4":
+      return "video/mp4";
+    default:
+      return "application/octet-stream";
+  }
+}
+
+function getStreamArguments(range: string, total: number) {
+  const positions = range.replace(/bytes=/, "").split("-");
+  const start = parseInt(positions[0], 10);
+  const end = positions[1] ? parseInt(positions[1], 10) : total - 1;
+  const chunkSize = (end - start) + 1;
+
+  return {
+    start, end, chunkSize
+  }
+}
